eb0ee57c245e6ee33b380ecb10733cc097e13f6b
seangan <871637188@qq.com>
2018-02-08 09:51:45 +0800

    修复user controller问题

--- code/eem-web/src/main/java/com/warrior/eem/controller/rest/UserManagerController.java
+++ code/eem-web/src/main/java/com/warrior/eem/controller/rest/UserManagerController.java
@@ -1,6 +1,7 @@
 package com.warrior.eem.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.warrior.eem.common.Result;
 import com.warrior.eem.entity.User;
+import com.warrior.eem.entity.vo.PageVo;
 import com.warrior.eem.entity.vo.UserCdtVo;
 import com.warrior.eem.entity.vo.UserVo;
 import com.warrior.eem.service.UserService;
@@ -18,33 +20,35 @@ import com.warrior.eem.service.UserService;
  * @author cold_blade
  * @version 1.0.0
  */
+@Controller
+@RequestMapping("user")
 public class UserManagerController extends AbstractController {
 
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "create", method = RequestMethod.POST)
+	@RequestMapping(value = "info", method = RequestMethod.POST)
 	@ResponseBody
-	public Result<Object> createEntity(@RequestBody UserVo userVo) {
+	public Result<Object> createEntity(@RequestBody(required = false) UserVo userVo) {
 		userService.createEntity(userVo);
 		return Result.success();
 	}
 
-	@RequestMapping(value = "update", method = RequestMethod.PUT)
+	@RequestMapping(value = "info", method = RequestMethod.PUT)
 	@ResponseBody
-	public Result<Object> updateEntity(@RequestBody UserVo userVo) {
+	public Result<Object> updateEntity(@RequestBody(required = false) UserVo userVo) {
 		userService.updateEntity(userVo);
 		return Result.success();
 	}
 
-	@RequestMapping(value = "delete", method = RequestMethod.DELETE)
+	@RequestMapping(value = "info", method = RequestMethod.DELETE)
 	@ResponseBody
 	public Result<Object> deleteEntity(String id) {
 		userService.deleteEntity(convertId(id));
 		return Result.success();
 	}
 
-	@RequestMapping(value = "query", method = RequestMethod.GET)
+	@RequestMapping(value = "info", method = RequestMethod.GET)
 	@ResponseBody
 	public Result<User> getEntity(String id) {
 		return Result.success((User) userService.getEntity(convertId(id)));
@@ -52,7 +56,8 @@ public class UserManagerController extends AbstractController {
 
 	@RequestMapping(value = "list", method = RequestMethod.POST)
 	@ResponseBody
-	public Result<Object> listEntities(@RequestBody UserCdtVo cdt) {
-		return Result.success(userService.listEntities(cdt));
+	public Result<Object> listEntities(@RequestBody(required = false) UserCdtVo cdt) {
+		PageVo pageVo = userService.listEntities(cdt);
+		return Result.success(pageVo.getCount(), pageVo.getDatas());
 	}
 }
