f0c0eeaa87b34224f99aea66d8ed80abb63deaa6
seangan <871637188@qq.com>
2018-02-08 10:41:54 +0800

    移除多余的售电合约月数据service 以及dao

--- code/eem-web/src/main/java/com/warrior/eem/dao/SellPowerAgreementMonthDataDao.java
+++
@@ -1,7 +0,0 @@
-package com.warrior.eem.dao;
-
-import com.warrior.eem.entity.SellPowerAgreementMonthData;
-
-public interface SellPowerAgreementMonthDataDao extends IDao<SellPowerAgreementMonthData> {
-	
-}
