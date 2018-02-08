de33cc0f2465436cbad2d09d6e1fa79ade14da8f
coldblade <cold_blade@126.com>
2018-02-08 14:26:05 +0800

    add role dao and service

--- code/eem-web/src/main/java/com/warrior/eem/dao/impl/RoleDaoImpl.java
+++ code/eem-web/src/main/java/com/warrior/eem/dao/impl/RoleDaoImpl.java
@@ -1,5 +1,7 @@
 package com.warrior.eem.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.warrior.eem.dao.RoleDao;
 import com.warrior.eem.entity.Role;
 
@@ -8,6 +10,7 @@ import com.warrior.eem.entity.Role;
  * @author cold_blade
  * @version 1.0.0
  */
+@Repository
 public class RoleDaoImpl extends AbstractDaoImpl<Role> implements RoleDao {
 
 	@Override
